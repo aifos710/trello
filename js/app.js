69b3e5e1aa6c00558bd8ccd41e8c92f7e1ddd105
aifos710 <sofia.m.caballero@gmail.com>
2016-09-22 18:11:57 -0500

    dos

--- js/app.js
+++ js/app.js
@@ -9,16 +9,43 @@ window.addEventListener("load", function() {
 		content.insertBefore(form, content.childNodes[0]);
 
 		var input = document.createElement("input");
-		form.insertBefore(input, form.childNodes[0]).classList.add("input");
+		form.insertBefore(input, form.childNodes[0]).classList.add("form-control");
+		input.placeholder = "Añadir una lista..."
 
 		var boton = document.createElement("button");
 		var texto = document.createTextNode("Guardar");
 		boton.appendChild(texto);
-		var boton2 = form.insertBefore(boton, form.childNodes[1]);
+		form.insertBefore(boton, form.childNodes[1]);
 
-		boton2.classList.add("btn-success");
-		boton2.classList.add("btn");
+		boton.classList.add("btn-success", "btn-block", "btn", "btn-sm", "pull-left");
+		boton.style.display = "inline-block";
 
-		content.classList.add("content2")
+		var icon = document.createElement("icon");
+		form.insertBefore(icon, form.childNodes[2]).classList.add("close");
+		icon.classList.add("icon-cross");
+		icon.classList.add("cross");
+
+		content.classList.add("content2");
+
+	boton.addEventListener("click", function(e) {
+		e.preventDefault();
+		var valorInput = input.value;
+		//ocultar todo display none
+		boton.style.display = "none";
+		input.style.display = "none";
+		icon.style.display = "none";
+		//crear otro div
+		var titulo = document.createElement("div");
+		titulo.innerHTML = valorInput;
+		content.insertBefore(titulo, content.childNodes[0]);
+		
+		var link = document.createElement("a");
+		var aText = document.createTextNode("Añadir una lista...")
+		link.appendChild(aText);
+		content.insertBefore(link, content.childNodes[1]);
+
+
+	})
+		
 	});
 });
