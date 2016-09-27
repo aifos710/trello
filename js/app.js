d7cf053b4ced464c077a50b906588480173ac08c
sofia caballero <sofia caballero>
2016-09-27 09:11:40 -0500

    cambios para la version 4

--- js/app.js
+++ js/app.js
@@ -1,51 +1,81 @@
-window.addEventListener("load", function() {
-	var content = document.getElementById("content");
-	var lista = document.getElementById("lista");
-
-	lista.addEventListener("click",function(){
-		lista.style.display = "none";
-
-		var form = document.createElement("form");
-		content.insertBefore(form, content.childNodes[0]);
-
-		var input = document.createElement("input");
-		form.insertBefore(input, form.childNodes[0]).classList.add("form-control");
-		input.placeholder = "Añadir una lista..."
-
-		var boton = document.createElement("button");
-		var texto = document.createTextNode("Guardar");
-		boton.appendChild(texto);
-		form.insertBefore(boton, form.childNodes[1]);
-
-		boton.classList.add("btn-success", "btn-block", "btn", "btn-sm", "pull-left");
-		boton.style.display = "inline-block";
-
-		var icon = document.createElement("icon");
-		form.insertBefore(icon, form.childNodes[2]).classList.add("close");
-		icon.classList.add("icon-cross");
-		icon.classList.add("cross");
-
-		content.classList.add("content2");
-
-	boton.addEventListener("click", function(e) {
-		e.preventDefault();
-		var valorInput = input.value;
-		//ocultar todo display none
-		boton.style.display = "none";
-		input.style.display = "none";
-		icon.style.display = "none";
-
-		//crear otro div
-		var titulo = document.createElement("div");
-		titulo.innerHTML = valorInput;
-		content.insertBefore(titulo, content.childNodes[0]);
-		
-		var link = document.createElement("a");
-		var aText = document.createTextNode("Añadir una lista...")
-		link.appendChild(aText);
-		content.insertBefore(link, content.childNodes[1]);
-
-	})
-		
+window.addEventListener("load", function() { 
+	
+	var section = document.getElementById("section"); 
+	var contenedor = document.getElementById("contenedor"); 
+	var lista = document.getElementById("lista"); 
+	var formulario = document.getElementById("formulario"); 
+	var tInput = document.getElementById("tInput"); 
+	var boton = document.getElementById("boton"); 
+	
+	lista.addEventListener("click", function() { 
+		this.style.display = "none"; 
+		formulario.style.display = "block"; 
+		this.parentElement.classList.add("caja"); 
+		tInput.focus();
 	});
-});
+
+	boton.addEventListener("click", function(e) { 
+		e.preventDefault(); 
+
+		addText(); 
+		addSection(); 
+	}); 
+
+	function addText() { 
+		
+		formulario.style.display = "none"; 
+		
+		var texto = tInput.value; 
+		var titulo = document.createElement("div"); 
+		
+		titulo.innerHTML = texto; 
+		lista.parentElement.insertBefore(titulo, lista.parentElement.childNodes[0]); 
+		titulo.classList.add("titulo"); 
+		tInput.value = ""; 
+		
+		var link = document.createElement("a"); 
+		var aText = document.createTextNode("Añadir una tarjeta..."); 
+		
+		link.appendChild(aText); 
+		lista.parentElement.insertBefore(link, lista.parentElement.childNodes[1]); 
+		link.classList.add("link"); 
+		link.setAttribute("href", "#"); 
+
+		link.addEventListener("click", function(){
+			addTextArea(this);
+		});
+	} 
+
+	function addSection() { 
+		
+		var nextRight = document.createElement("div"); 
+		
+		section.appendChild(nextRight); 
+		nextRight.classList.add("nextRight"); 
+		nextRight.appendChild(lista); 
+		lista.style.display = "block"; 
+		nextRight.insertBefore(formulario, nextRight.childNodes[0]); 
+		}
+
+	function addTextArea(link){
+		
+		link.style.display = "none";
+
+		var formularioTextArea = document.createElement("form");
+		link.parentElement.appendChild(formularioTextArea);
+
+		var TextArea = document.createElement("TextArea");
+		formularioTextArea.insertBefore("textArea", formularioTextArea.childNodes[0]);
+		TextArea.classList.add("targetText");
+
+		var tBoton = document.createElement("button");
+		formularioTextArea.insertBefore(TbOTON, formularioTextArea.childNodes[1]);
+
+		var tBoton = document.createTextNode("Añadir");
+		tBoton.appendChild(tBoton);
+		tBoton.classList.add("button");
+		tBoton.setAttribute("type", "submit");
+		TextArea.focus();
+	}
+
+	});  
